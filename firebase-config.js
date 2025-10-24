f097802996b73b7ad80c6abdf781afba156774b0
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-10-24 08:39:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -23,14 +23,14 @@ import {
 } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
 // Your web app's Firebase configuration
-// Replace with your actual Firebase config
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+  apiKey: "AIzaSyCHmN3ChQm8HLMQ1bxzwtd7dEaJJZDPts4",
+  authDomain: "graminchits.firebaseapp.com",
+  projectId: "graminchits",
+  storageBucket: "graminchits.firebasestorage.app",
+  messagingSenderId: "775668330555",
+  appId: "1:775668330555:web:8585a06c781ae856ceac1e",
+  measurementId: "G-9KWY7YMSBQ"
 };
 
 // Initialize Firebase
@@ -58,3 +58,4 @@ export {
     orderBy,
     serverTimestamp 
 };
+
